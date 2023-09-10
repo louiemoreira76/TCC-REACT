e074f439a07101a3bf29d950cf19ebf85fe112e1
FelipePereira07 <129508280+FelipePereira07@users.noreply.github.com>
2023-09-10 19:05:43 -0300

    Pagina Conquistas

--- site/src/Routes.js
+++ site/src/Routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Produto from './pages/produto';
 import Planos from './pages/planos';
+import Conquistas from './pages/conquistas';
 
 export default function Routess(){
     return(
@@ -9,6 +10,7 @@ export default function Routess(){
             <Routes>
                 <Route path="/" element={<Produto/>} />
                 <Route path='/planos' element={<Planos/>} />
+                <Route path='/conquistas' element={<Conquistas/>} />
             </Routes>
         </BrowserRouter>
     )
