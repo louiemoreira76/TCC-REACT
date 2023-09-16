bff95949bb8c36444b630b84234d748445094f66
Luis <luis010101work@outlook.com>
2023-09-16 16:31:49 -0300

    perfil mudar

--- site/src/Routes.js
+++ site/src/Routes.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Produto from './pages/produto';
 import Planos from './pages/planos';
 import Conquistas from './pages/conquistas';
+import MudarPerfil from './pages/changes';
 
 export default function Routess(){
     return(
@@ -11,6 +12,7 @@ export default function Routess(){
                 <Route path="/" element={<Produto/>} />
                 <Route path='/planos' element={<Planos/>} />
                 <Route path='/conquistas' element={<Conquistas/>} />
+                <Route path='/perfil' element={<MudarPerfil/>} />
             </Routes>
         </BrowserRouter>
     )
