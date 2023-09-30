d496edc38459ea1795d33adbca7669a63fed8a5a
Luis <luis010101work@outlook.com>
2023-09-29 22:35:34 -0300

    admin login e services

--- site/src/Routes.js
+++ site/src/Routes.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NotFound from './errors';
 
 import Produto from './pages/web/produto';
 import Planos from './pages/web/planos';
 import Conquistas from './pages/web/conquistas';
 import MudarPerfil from './pages/web/changes';
 import Navegar from './pages/web/navegar';
-
 import GameSync from './pages/web/GameSync';
 import Descobrir from './pages/web/descobrir';
 
-import CadastroAdmin from './pages/admin/login_admin'
-
+import CadastroAdmin from './pages/admin/authentication'
+import HomeAdmin from './pages/admin/home';
 
 export default function Routess(){
     return(
@@ -24,7 +24,10 @@ export default function Routess(){
                 <Route path='/procurar' element={<Navegar/>} />
                 <Route path='/sobregamesync' element={<GameSync/>} />
 
+                <Route path='/admin/home' element={<HomeAdmin/>} />
                 <Route path='/admin' element={<CadastroAdmin/>} />
+
+                <Route path='*' element={<NotFound/>} />
             </Routes>
         </BrowserRouter>
     )
--- site/src/index.js
+++ site/src/index.js
@@ -5,9 +5,14 @@ import ReactDOM from 'react-dom/client';
 
 import Routess from './Routes.js';
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+      <ToastContainer />
       <Routess />
   </React.StrictMode>
 );
